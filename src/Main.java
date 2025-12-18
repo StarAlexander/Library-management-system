e912a297e4d46f61c22313d7a1acb0f2cfab8ead
Alexander <starchenkoa3@gmail.com>
2025-12-18 11:05:36 +0300

    Added Main class skeleton

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-
-
+import library.Book;
+import library.Library;
 
 public class Main {
     public static void main(String[] args) {
+        Library library = new Library();
 
-        }
+
+    }
     }
