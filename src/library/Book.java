dcc8be32ffb6f3716ce372a58f38992adcef297e
Alexander <starchenkoa3@gmail.com>
2025-12-18 10:30:33 +0300

    Added Book class with basic fields and methods

--- src/library/Book.java
+++ src/library/Book.java
@@ -1,4 +1,60 @@
 package library;
 
 public class Book {
+    private final int id;
+
+    private final String title;
+    private final String author;
+
+    private final int year;
+    private final String isbn;
+    private boolean available;
+
+    public Book(int id, String title, String author, int year, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", isbn='" + isbn + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }
